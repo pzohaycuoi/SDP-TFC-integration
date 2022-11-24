439881d0973fbca449a4cce0286d61dd8104d8b2
pzohaycuoi <pzohaycuoi@gmail.com>
2022-11-24 14:01:18 +0700

    update TerraformApi.py: more clear comment and add workspace_get func

--- TerraformSDPIntegration/TerraformApi.py
+++ TerraformSDPIntegration/TerraformApi.py
@@ -2,15 +2,17 @@ import requests
 from os.path import exists
 
 
-def creation(token: str, terraform_org: str, workspace_name: str, auto_apply: bool):
+def workspace_create(token: str, terraform_org: str, workspace_name: str, auto_apply=False):
     """
-    Construct API payload for Terraform workspace creation.
+    Create Terraform workspace
+    Construct API request and send API request for Terraform workspace creation.
     https://developer.hashicorp.com/terraform/cloud-docs/api-docs/workspaces
-    :param terraform_org:
-    :param token:
-    :param workspace_name: string
-    :param auto_apply: bool
-    :return: dict
+    :param terraform_org: Terraform organization name
+    :param token: Terraform user token has enough permission to create workspace
+    :param workspace_name: Terraform workspace name
+    :param auto_apply: Terraform workspace auto-apply setting, if set to True,Terraform will run apply
+    after success Terraform run plan.
+    :return: API response
     """
     payload = {"data": {"attributes": {"name": workspace_name, "description": "Created by API call",
                                        "auto_apply": auto_apply}, "type": "workspaces"}}
@@ -24,14 +26,36 @@ def creation(token: str, terraform_org: str, workspace_name: str, auto_apply: bo
     return req
 
 
-def config(token: str, workspace_id: str, auto_queue: bool):
+def workspace_get(token: str, terraform_org: str, workspace_name: str):
     """
-    Construct API payload for config Terraform workspace configuration versions
+    Get Terraform workspace information through API request
+    Construct API request and send API request for getting the workspace information
+    https://developer.hashicorp.com/terraform/cloud-docs/api-docs/workspaces
+    :param terraform_org: Terraform organization name
+    :param token: Terraform user token
+    :param workspace_name: Terraform workspace name
+    :return: API response
+    """
+    header = {"Content-type": "application/vnd.api+json", "Authorization": f"Bearer {token}"}
+    url = f"https://app.terraform.io/api/v2/organizations/{terraform_org}/workspaces/{workspace_name}"
+    try:
+        req = requests.post(url, headers=header, verify=True)
+    except requests.exceptions.RequestException as err:
+        raise SystemExit(err)
+
+    return req
+
+
+def workspace_config_create(token: str, workspace_id: str, auto_queue=False):
+    """
+    Create Terraform workspace configuration version for Terraform workspace doesn't attach to any VCS (Git/Gitlab repo)
+    Construct API request and send API request for creation of Terraform workspace configuration versions
     https://developer.hashicorp.com/terraform/cloud-docs/api-docs/configuration-versions#create-a-configuration-version
-    :param token:
-    :param workspace_id:
-    :param auto_queue:
-    :return:
+    :param token: Terraform user token
+    :param workspace_id: Terraform workspace ID, can get this ID using get_workspace function or after Terraform
+    workspace creation
+    :param auto_queue: if set True, Terraform automatically queues runs when new commits are merged.
+    :return: API response
     """
     payload = {"data": {"type": "configuration-versions", "attributes": {"auto-queue-runs": auto_queue}}}
     header = {"Content-type": "application/vnd.api+json", "Authorization": f"Bearer {token}"}
@@ -44,14 +68,17 @@ def config(token: str, workspace_id: str, auto_queue: bool):
     return req
 
 
-def upload_code(token: str, filepath: str, upload_url: str):
+def workspace_upload_code(token: str, filepath: str, upload_url: str):
     """
-    Construct API payload for config Terraform workspace configuration versions
+    Upload Terraform code in .tar.gz type, to Terraform configuration version
+    Construct API request and send API request for uploading Terraform .tar.gz file
+    to Terraform workspace configuration version
     https://developer.hashicorp.com/terraform/cloud-docs/api-docs/configuration-versions#upload-configuration-files
-    :param upload_url:
-    :param filepath:
-    :param token:
-    :return:
+    :param upload_url: Terraform configuration version url, can get this from Terraform workspace configuration version
+    creation or workspace_config_get function
+    :param filepath: path to .tar.gz contains Terraform code files
+    :param token: Terraform user token
+    :return: API response
     """
     header = {"Content-type": "application/octet-stream", "Authorization": f"Bearer {token}"}
     url = upload_url
