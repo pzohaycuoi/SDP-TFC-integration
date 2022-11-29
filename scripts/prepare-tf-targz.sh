2c6435884e67b878fdf54bc44130007245a676ea
pzohaycuoi <pzohaycuoi@gmail.com>
2022-11-29 14:08:26 +0700

    remove scripts folder

--- scripts/prepare-tf-targz.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/pzohaycuoi/learn-terraform-no-code-provisioning.git
-cd learn-terraform-no-code-provisioning || exit
-git archive --format=tar.gz  -o ../repo.tar.gz --prefix=learn-terraform-no-code-provisioning/ main
-cd .. || exit
-rm -rf learn-terraform-no-code-provisioning
